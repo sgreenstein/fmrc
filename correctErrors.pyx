c6a66e4c26e45229e03221400bffaa0f8e5348c8
Seth Greenstein <sgreens@cs.unc.edu>
2015-06-24 13:45:12 -0400

    Uses NoOther query from msbwt, has other small optimizations

--- correctErrors.pyx
+++ correctErrors.pyx
@@ -1,4 +1,5 @@
 from MUSCython import MultiStringBWTCython as msbwt, LCPGen
+from MUSCython.BasicBWT cimport BasicBWT
 from time import clock
 from tempfile import NamedTemporaryFile
 import logging
@@ -17,7 +18,7 @@ NUM_TO_BASE = ['A', 'C', 'G', 'N', 'T', '$']
 
 TRAN_TAB = maketrans('ACGT', 'TGCA')
 
-def reverseComplement(seq):
+cdef bytes reverseComplement(seq):
     return seq[::-1].translate(TRAN_TAB)
 
 
@@ -29,19 +30,18 @@ def fastaParser(fasta, int start, int end):
         for _ in xrange(start, end):
             yield fp.next(), fp.next(), fp.next(), fp.next()
 
-
+@cython.boundscheck(False)
 def correct(inFastqFile, bwtDir, k, hiThresh, numProcesses, processNum):
     begin = clock()
-    bwt = msbwt.loadBWT(bwtDir, False)
+    cdef BasicBWT bwt = msbwt.loadBWT(bwtDir, False)
     cdef np.ndarray[np.uint8_t] corrected
     cdef np.ndarray[np.uint8_t] trusted
     cdef np.ndarray[np.uint64_t] counts
     cdef np.ndarray[np.uint64_t] revCounts
     cdef np.ndarray[np.uint64_t] newCounts
-    cdef np.ndarray[np.int64_t] _
     cdef int readsPerProcess = (bwt.getSymbolCount(0) / numProcesses) + 1
     cdef int kmersEnd, kmersStart
-    cdef long lo, hi, rcLo, rcHi, bestSupport
+    cdef long lo, hi, rcLo, rcHi, bestSupport, support
     cdef bytes readName, origRead, plus, qual, base, bestBase
     cdef bint changeMade
     tmpFile = NamedTemporaryFile(suffix='.fastq', delete=False)
@@ -52,10 +52,8 @@ def correct(inFastqFile, bwtDir, k, hiThresh, numProcesses, processNum):
         for readName, origRead, plus, qual in \
                 fastaParser(inFastqFile, readsPerProcess*processNum,
                             min(readsPerProcess * (processNum+1), bwt.getSymbolCount(0))):
-            read = list(origRead[:-1])
-            revCounts, _ = bwt.countStrandedSeqMatches(reverseComplement(origRead[:-1]), k)
-            # if revCounts  != bwt.countStrandedSeqMatchesNoOther(reverseComplement(origRead[:-1]), k):
-            #     print 'Not the same'
+            read = list(origRead[:len(origRead)-1])
+            revCounts = bwt.countStrandedSeqMatchesNoOther(reverseComplement(origRead[:len(origRead)-1]), k)
             revCounts = np.flipud(revCounts)
             trusted = (revCounts > 0).astype(np.uint8)
             corrected = np.zeros(len(read), dtype=np.uint8)
@@ -63,9 +61,7 @@ def correct(inFastqFile, bwtDir, k, hiThresh, numProcesses, processNum):
             # if not readsDone & 1023:
             #     logging.debug('Finished %d reads', readsDone)
             if False in trusted:
-                counts, _ = bwt.countStrandedSeqMatches(origRead[:-1], k)
-                # if counts != bwt.countStrandedSeqMatchesNoOther(origRead[:-1], k):
-                #     print 'Not the same'
+                counts = bwt.countStrandedSeqMatchesNoOther(origRead[:len(origRead)-1], k)
                 trusted |= counts > hiThresh
                 changeMade = True
                 while changeMade:
@@ -79,47 +75,45 @@ def correct(inFastqFile, bwtDir, k, hiThresh, numProcesses, processNum):
                                     for base in BASES:
                                         rcLo = bwt.getOccurrenceOfCharAtIndex(BASE_NUMS[base.translate(TRAN_TAB)], newLo)
                                         rcHi = bwt.getOccurrenceOfCharAtIndex(BASE_NUMS[base.translate(TRAN_TAB)], newHi)
-                                        lo, hi = bwt.findIndicesOfStr(''.join(read[i+1:i+k]) + base)
-                                        # rcLo, rcHi = bwt.findIndicesOfStr(reverseComplement(''.join(read[i+1:i+k]) + base))
-                                        if hi - lo + rcHi - rcLo > bestSupport:
-                                            bestSupport = hi - lo + rcHi - rcLo
+                                        lo, hi = bwt.findIndicesOfStr(<bytes> ''.join(read[i+1:i+k]) + base)
+                                        support = hi - lo + rcHi - rcLo
+                                        if support > bestSupport:
+                                            bestSupport = support
                                             bestBase = base
                                     if bestBase != read[i+k]:
                                         changeMade = True
                                         corrected[i+k] = True
                                         read[i+k] = bestBase
-                                    if corrected[i+k]:
                                         kmersEnd = min(len(read), i+2*k)
-                                        newCounts, _ = bwt.countStrandedSeqMatches(
+                                        newCounts = bwt.countStrandedSeqMatchesNoOther(
                                             reverseComplement(''.join(read[i+1:kmersEnd])), k)
                                         newCounts = np.flipud(newCounts)
                                         trusted[i+1:kmersEnd-k+1] = newCounts > 0
                                         if False in trusted[i+1:kmersEnd-k+1]:
-                                            newCounts, _ = bwt.countStrandedSeqMatches(''.join(read[i+1:kmersEnd]), k)
+                                            newCounts = bwt.countStrandedSeqMatchesNoOther(<bytes> ''.join(read[i+1:kmersEnd]), k)
                                             trusted[i+1:kmersEnd-k+1] |= newCounts > hiThresh
                             elif trusted[i+1] and not corrected[i]:  # err at read[i]
                                 bestSupport = 0
-                                newLo, newHi = bwt.findIndicesOfStr(''.join(read[i+1:i+k]))
+                                newLo, newHi = bwt.findIndicesOfStr(<bytes> ''.join(read[i+1:i+k]))
                                 for base in BASES:
                                     lo = bwt.getOccurrenceOfCharAtIndex(BASE_NUMS[base], newLo)
                                     hi = bwt.getOccurrenceOfCharAtIndex(BASE_NUMS[base], newHi)
-                                    # lo, hi = bwt.findIndicesOfStr(base + ''.join(read[i+1:i+k]))
                                     rcLo, rcHi = bwt.findIndicesOfStr(reverseComplement(base + ''.join(read[i+1:i+k])))
-                                    if hi - lo + rcHi - rcLo > bestSupport:
-                                        bestSupport = hi - lo + rcHi - rcLo
+                                    support = hi - lo + rcHi - rcLo
+                                    if support > bestSupport:
+                                        bestSupport = support
                                         bestBase = base
                                 if bestBase != read[i]:
                                     changeMade = True
                                     corrected[i] = True
                                     read[i] = bestBase
-                                if corrected[i]:
                                     kmersStart = max(0, i-k)
-                                    newCounts, _ = bwt.countStrandedSeqMatches(
+                                    newCounts = bwt.countStrandedSeqMatchesNoOther(
                                         reverseComplement(''.join(read[kmersStart:i+k])), k)
                                     newCounts = np.flipud(newCounts)
                                     trusted[kmersStart:i+1] = newCounts > 0
                                     if False in trusted[kmersStart:i+1]:
-                                        newCounts, _ = bwt.countStrandedSeqMatches(''.join(read[kmersStart:i+k]), k)
+                                        newCounts = bwt.countStrandedSeqMatchesNoOther(<bytes> ''.join(read[kmersStart:i+k]), k)
                                         trusted[kmersStart:i+1] |= newCounts > hiThresh
             tmpFile.write(readName)
             tmpFile.write(''.join(read) + '\n')
@@ -154,14 +148,15 @@ def buildLCP(bwtDir, maxReadLen):
     logging.basicConfig(level=logging.DEBUG)
     logging.info('Building LCP array')
     begin = clock()
-    np.save(bwtDir + 'lcps.npy', LCPGen.lcpGenerator(bwtDir, maxReadLen+1, logging.getLogger()))
+    cdef np.ndarray[np.uint8_t] lcps = LCPGen.lcpGenerator(bwtDir, maxReadLen+1, logging.getLogger())
+    np.save(bwtDir + 'lcps.npy', lcps)
     logging.info('Finished building LCP in %d s', clock() - begin)
 
 
 def main():
     # buildLCP('/playpen/sgreens/fq_celegans/msbwt/bwt/', 101)
-    # willBeMain('/playpen/sgreens/ecoli/EAS20_8/cov20.txt', '/playpen/sgreens/ecoli/msbwt20/rle_bwt/',
-    #            outFilename='/playpen/sgreens/ecoli/msbwt20/corrected.fastq', numProcesses=4)
-    prefix = '/playpen/sgreens/fq_celegans/'
-    willBeMain(prefix + 'cov10.txt', prefix + '/msbwt/bwt/', 101,
-               outFilename=prefix+'/msbwt/corrected.fastq', numProcesses=4)
+    willBeMain('/playpen/sgreens/ecoli/EAS20_8/cov20.txt', '/playpen/sgreens/ecoli/msbwt20/rle_bwt/', 101,
+               outFilename='/playpen/sgreens/ecoli/msbwt20/corrected.fastq', numProcesses=4)
+    # prefix = '/playpen/sgreens/fq_celegans/'
+    # willBeMain(prefix + 'cov10.txt', prefix + '/msbwt/bwt/', 101,
+    #            outFilename=prefix+'/msbwt/corrected.fastq', numProcesses=4)
